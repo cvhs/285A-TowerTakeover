819fdb4437f2f64bd01a0700b899869215aa8196
Ankush Girotra <ankush.girotra@gmail.com>
2020-02-20 16:23:59 -0600

    blue right auto

--- src/autons/blueRight.cpp
+++ src/autons/blueRight.cpp
@@ -3,5 +3,22 @@
 #include "../assistants/assistants.h"
 
 void blueRight() {
-  
+  setIntakePct(100);
+  moveForward(600, 15);
+  vex::task::sleep(200);
+  turnClockwise(220, 30);
+  vex::task::sleep(200);
+  moveForward(600, 41);
+  vex::task::sleep(200);
+  turnClockwise(110, 30);
+  vex::task::sleep(200);
+  moveForward(300, 41);
+  vex::task::sleep(200);
+  intakeMove(-500);
+  vex::task::sleep(500);
+  liftCubes();
+  moveForward(40, 20);
+  vex::task::sleep(200);
+  backOut();
+  vex::task::sleep(500);
 }
